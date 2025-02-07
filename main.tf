c468eeec26ad425d54be296a048711fa5933ee42
Ivan Gabrovski <igabrpro88@gmail.com>
2025-02-07 09:01:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,52 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.93.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # disable automatic registration of Azure resource provider
+  skip_provider_registration = true
+
+}
+
+# Create resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "TFResourceGroup"
+  location = "southcentralus"
+}
+
+# Create VNET
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+
 module "repo1" {
   source = "./repo1"
 }
